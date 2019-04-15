23fc979ad49849c1dead7f8a4887ed9f0fe9ae51
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2019-04-16 01:21:32 +0200

    uva 11953

---
+++ UVA/UVA11953.cpp
@@ -0,0 +1,90 @@
+// Fub0ki
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long             ll;
+typedef vector<int>           vi;
+typedef vector<ll>            vll;
+typedef pair<int, int>        ii;
+typedef vector<ii>            vii;
+typedef set<int>              si;
+typedef map<string, int>      msi;
+typedef map<string, string>   mss;
+#define MEM(x) memset(x,0,sizeof(x))
+#define MEMS(x) memset(x,-1,sizeof(x))
+#define mp make_pair
+#define pb push_back
+#define mt make_tuple
+#define eb emplace_back
+#define bn begin()
+#define nd end()
+#define X first
+#define Y second
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define NumofDigits(n) ((int)log10(n)+1)
+#define scn(x)  scanf("%d",&x)
+#define lop(i, a, b) for (int i = int(a); i < int(b); i++)
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define MAX 100005
+#define INF 1e18
+#define print_prec(ans,num) cout << fixed << setprecision(num) << (ans) << endl
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL)
+const int dx[] = { 1,-1,0,0,0,0 };
+const int dy[] = { 0,0,1,-1,0,0 };
+const int dz[] = { 0,0,0,0,1,-1 };
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistic*/
+/*max_element(all(m),
+    [](const ii& p1, const ii& p2) {
+        return p1.second < p2.second; });*/
+int n,ans,t;
+string grid[101];
+void dfs(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= n)
+    {
+        return;
+    }
+    if (grid[i][j] == '.')
+    {
+        return;
+    }
+    grid[i][j] = '.';
+    for (int cnt = 0; cnt < 4; ++cnt)
+    {
+        dfs(i+dx[cnt],j+dy[cnt]);
+    }
+}
+int main()
+{
+    //freopen("read.txt", "r", stdin);
+    //freopen("write.txt", "w", stdout);
+    //IO;
+    scn(t);
+    for(int test = 1; test <= t; test++)
+    {
+        ans = 0;
+        scn(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> grid[i];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (grid[i][j] == 'x')
+                {
+                    ans++;
+                    dfs(i,j);
+                }
+            }
+        }
+        printf("Case %d: %d\n",test,ans);
+    }
+    return 0;
+}
